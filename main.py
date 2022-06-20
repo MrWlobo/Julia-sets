e40deb6f5deb79ddeb8cc99a1394393e88633547
MrWlobo <michaldworniczak40@gmail.com>
2022-06-20 21:33:06 +0200

    created functions for updating screen and displaying the set

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pygame
-from variables import WIDTH, HEIGHT
+from variables import WIDTH, WIN, black_pixel
+from julia_set import output, select_point
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Julia sets displayer")
+
+def value_choice():
+    index = 0
+    for char in output:
+        if char != ".":
+            WIN.blit(black_pixel, (int(index) % int(WIDTH), index // WIDTH))
+        index += 15
+
+def update(event):
+    select_point(event)
+    value_choice()
+    pygame.display.update()
 
 def main():
     run = True
@@ -9,5 +22,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        update(event)
     pygame.quit()
 main()

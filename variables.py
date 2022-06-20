c59561044595bbfe4623d3d49d661dc32fa48fed
MrWlobo <michaldworniczak40@gmail.com>
2022-06-20 21:13:21 +0200

    Added colors and sprites

--- variables.py
+++ variables.py
@@ -1,1 +1,16 @@
-WIDTH, HEIGHT = 500, 500
+import pygame
+
+#Measures
+WIDTH, HEIGHT = 400, 400
+WINDOW_SCALE = 4
+
+#Window
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+#Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+#Sprites
+black_pixel = pygame.Surface([16, 16])
+black_pixel.fill(WHITE)

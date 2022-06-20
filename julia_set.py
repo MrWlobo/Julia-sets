37cc098b87a3a083b3715aa7bd6f3c2328a90758
MrWlobo <michaldworniczak40@gmail.com>
2022-06-20 21:33:57 +0200

    Value will now be chosen by clicking on the application screen

--- julia_set.py
+++ julia_set.py
@@ -1,19 +1,23 @@
+import pygame
+from variables import WIDTH, HEIGHT, WINDOW_SCALE, WIN, BLACK
+
 #Positions of pixels
 pixels = []
 real_number, imaginary_number = 0, 0 #Position X, Position Y
 
 #Give every pixel a positiom
-for i in range(63001):
+for i in range(WIDTH // WINDOW_SCALE * HEIGHT // WINDOW_SCALE):
     pixels.append((real_number, imaginary_number))
     real_number += 1
-    if real_number == 251:
+    if real_number == WIDTH:
         real_number = 0
         imaginary_number += 1
 
 #Imaginary and real values of pixels
+BASE_VALUE = 200
 values = []
-imaginary_value = 2000
-real_value = -2000
+imaginary_value = BASE_VALUE
+real_value = -1 * BASE_VALUE
 change_value = 16
 iterations = 0
 
@@ -21,15 +25,15 @@ for pixel in pixels:
     values.append((real_value, imaginary_value))
     real_value += change_value
     iterations += 1
-    if iterations == 251:
+    if iterations == WIDTH:
         iterations = 0
-        real_value = -2000
+        real_value = -1 * BASE_VALUE
         imaginary_value -= change_value
 
 output = []
 
 def create_julia_set(value, c):
-    z = complex(value[0]/1000, value[1]/1000) #Check for every pixel
+    z = complex(value[0]/BASE_VALUE*2, value[1]/BASE_VALUE*2) #Check for every pixel
     is_in_set = False
     solutions = []
 
@@ -41,7 +45,7 @@ def create_julia_set(value, c):
             output.append(".")
             break
 
-        if len(solutions) > 10 and len(solutions) < 100:
+        if len(solutions) > 5 and len(solutions) < 10:
             #Point might be in set
             output.append("o")
             break
@@ -55,18 +59,15 @@ def create_julia_set(value, c):
                 output.append("0")
         solutions.append(solution)
 
-print("Format: float + float") #Example: 0.24 + 0.45
-complex_value_input = str(input())
-complex_value_elements = complex_value_input.split()
-complex_value = complex(float(complex_value_elements[0]), float(complex_value_elements[2]))
+def select_point(event):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if pygame.mouse.get_pressed()[0]:
+            mouse_position = pygame.mouse.get_pos()
+            WIN.fill(BLACK)
+            output.clear()
+            complex_value = complex(float(mouse_position[0]/BASE_VALUE - 1), float(-1 * (mouse_position[1]/BASE_VALUE - 1)))
 
-#Displays anwser and write it to text file
-for value in values:
-    index = values.index(value)
-    create_julia_set(value, complex_value)
-    if len(output) == 251:
-        for char in output:
-            print(char, end="")
-        print("\n")
-        output.clear()
-
+            #Displays anwser and write it to text file
+            for value in values:
+                index = values.index(value)
+                create_julia_set(value, complex_value)

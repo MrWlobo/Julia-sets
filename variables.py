e8f7ba03bee74dde66cef5e1c0806f6f4a77b81f
MrWlobo <michaldworniczak40@gmail.com>
2022-06-21 19:46:41 +0200

    Added variables for displaying selected values

--- variables.py
+++ variables.py
@@ -1,16 +1,25 @@
 import pygame
 
 #Measures
-WIDTH, HEIGHT = 400, 400
-WINDOW_SCALE = 4
+WINDOW_SIZE = 800
+WIDTH, HEIGHT, BOTTOM_SURFACE_HEIGHT = WINDOW_SIZE, WINDOW_SIZE, WINDOW_SIZE // 8
+WINDOW_SCALE = 2
+SQUARE_SIZE = WINDOW_SIZE // 200
+PADDING = 10
 
 #Window
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT + BOTTOM_SURFACE_HEIGHT))
 
 #Colors
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+GREEN = (15, 210, 45)
+GREY = (45, 45, 45)
 
 #Sprites
-black_pixel = pygame.Surface([16, 16])
-black_pixel.fill(WHITE)
+green_pixel = pygame.Surface([SQUARE_SIZE, SQUARE_SIZE])
+green_pixel.fill(GREEN)
+bottom_surface = pygame.Surface([WIDTH, BOTTOM_SURFACE_HEIGHT])
+bottom_surface.fill(GREY)
+
+#Chosen values
+chosen_values = ["-", "-"]

802c334507907f9d8d43a78b9bc94b8a09b532f9
MrWlobo <michaldworniczak40@gmail.com>
2022-06-21 19:31:51 +0200

    Asigning chosen values to a list in variables.py, changed 'pixels' to 'squares'

--- julia_set.py
+++ julia_set.py
@@ -1,27 +1,27 @@
 import pygame
-from variables import WIDTH, HEIGHT, WINDOW_SCALE, WIN, BLACK
+from variables import WIDTH, HEIGHT, WINDOW_SCALE, WIN, WINDOW_SIZE, BLACK, chosen_values
 
-#Positions of pixels
-pixels = []
+#Positions of squares
+squares = []
 real_number, imaginary_number = 0, 0 #Position X, Position Y
 
-#Give every pixel a positiom
+#Give every square a positiom
 for i in range(WIDTH // WINDOW_SCALE * HEIGHT // WINDOW_SCALE):
-    pixels.append((real_number, imaginary_number))
+    squares.append((real_number, imaginary_number))
     real_number += 1
     if real_number == WIDTH:
         real_number = 0
         imaginary_number += 1
 
-#Imaginary and real values of pixels
-BASE_VALUE = 200
+#Imaginary and real values of squares
+BASE_VALUE = WINDOW_SIZE * 2
 values = []
 imaginary_value = BASE_VALUE
 real_value = -1 * BASE_VALUE
-change_value = 16
+change_value = WINDOW_SCALE * 8
 iterations = 0
 
-for pixel in pixels:
+for square in squares:
     values.append((real_value, imaginary_value))
     real_value += change_value
     iterations += 1
@@ -32,22 +32,22 @@ for pixel in pixels:
 
 output = []
 
-def create_julia_set(value, c):
-    z = complex(value[0]/BASE_VALUE*2, value[1]/BASE_VALUE*2) #Check for every pixel
+def create_julia_set(value, complex_value):
+    z = complex(value[0]/BASE_VALUE*2, value[1]/BASE_VALUE*2) #Check for every square
     is_in_set = False
     solutions = []
 
     while is_in_set == False:
-        solution = (z * z) + c
+        solution = (z*z) + complex_value
 
         if solution.real >= 2 or solution.imag >= 2:
             #Point is not in set
             output.append(".")
             break
 
-        if len(solutions) > 5 and len(solutions) < 10:
-            #Point might be in set
-            output.append("o")
+        # Point might be in set
+        if len(solutions) > 6:
+            output.append("1")
             break
 
         z = solution
@@ -56,18 +56,20 @@ def create_julia_set(value, c):
             if sol == solution:
                 #Point is in set
                 is_in_set = True
-                output.append("0")
+                output.append("1")
         solutions.append(solution)
 
 def select_point(event):
     if event.type == pygame.MOUSEBUTTONDOWN:
         if pygame.mouse.get_pressed()[0]:
             mouse_position = pygame.mouse.get_pos()
-            WIN.fill(BLACK)
-            output.clear()
-            complex_value = complex(float(mouse_position[0]/BASE_VALUE - 1), float(-1 * (mouse_position[1]/BASE_VALUE - 1)))
+            #Proceed if clicked on displaying surface
+            if mouse_position[1] < HEIGHT:
+                WIN.fill(BLACK)
+                output.clear()
+                complex_value = complex(float(4*mouse_position[0]/WINDOW_SIZE - 2), float(-4*(mouse_position[1]/WINDOW_SIZE) + 2))
+                chosen_values[0], chosen_values[1] = str(complex_value.real), str(complex_value.imag)
 
-            #Displays anwser and write it to text file
-            for value in values:
-                index = values.index(value)
-                create_julia_set(value, complex_value)
+                #Displays anwser and write it to text file
+                for value in values:
+                    create_julia_set(value, complex_value)
